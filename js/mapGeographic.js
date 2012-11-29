5fdf8d75330255c664cb5d9e84c7b83fef58cf26
Pasha <pmashchenko@gmail.com>
2012-11-29 16:38:51 +0200

    add animation map

--- js/mapGeographic.js
+++ js/mapGeographic.js
@@ -3,14 +3,57 @@ $(document).ready(function(){
         color: '-linear-gradient(#c2c2c2, #383838, #000000)'
     });
 
-    $('#map-control').on('click', 'a', function (evt){
+    var fadeSpeedFast = 200;
+    var fadeSpeedSlow =700;
+    var animateSpeed = 1000;
+
+    $('#map-control').on('click', 'a', function (){
     	$(this).addClass('active').siblings('a').removeClass('active');
+        showMapSlide();
+        return false;
     });
 
     var map = $('#mapGeographic');
-    $('> *', map).css('display', 'none');
 
-    map.find('#map-part-left, #map-part-right').fadeIn(1000, function (){
-    	map.find('#map-control').fadeIn(200);
+    map.fadeIn(fadeSpeedSlow,function(){
+        map.find('#map-control').fadeIn(fadeSpeedFast, function(){
+            map.find('ul').fadeIn(fadeSpeedSlow, function(){
+                map.find('#gas-pipe').fadeIn(fadeSpeedSlow, function(){
+                    map.find('#gas-count').fadeIn(fadeSpeedSlow);
+                });
+            });
+        });
     });
+
+    function showMapSlide(){
+        map.find('#gas-count, #gas-pipe, ul, h2').fadeOut(fadeSpeedSlow, function(){
+            map.find('#map-part-left, #map-part-right').css('visibility', 'visible');
+            map.css('background', 'none');
+            var mapLeftPosition = parseInt(map.find('#map-part-left').css('left'));
+            var mapRightPosition = parseInt(map.find('#map-part-right').css('left'));
+
+            map.find('#map-part-left').animate({left: mapLeftPosition-24}, animateSpeed);
+            map.find('#map-part-right').animate({left: mapRightPosition+47}, animateSpeed, function(){
+                map.find('#gas-count .textGrad').html('104 720 000 000').siblings('p').html('запасов кубометров газа');
+                Cufon.replace('.textGrad', {
+                    color: '-linear-gradient(#c2c2c2, #383838, #000000)'
+                });
+                map.find('h2').html('Люди Ямала – мощная сила!').fadeIn(fadeSpeedSlow);
+                $('#gas-field',this).fadeIn(fadeSpeedSlow);
+                map.find('#gas-count').fadeIn(fadeSpeedSlow);
+
+            });
+        });
+//        map.find('ul').fadeOut(fadeSpeedSlow);
+
+
+
+        //map.find('#map-part-right').animate({left: 10}, animateSpeed);
+    }
+
+   /* setTimeout(function(){
+        map.find('#map-part-left, #map-part-right').css('visibility', 'visible');
+        map.css('background', 'none');
+    },1000);*/
+
 });
