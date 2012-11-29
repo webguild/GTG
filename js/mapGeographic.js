f48b690f45ee44f51c9b4ea424d878e7871b717b
Mashchenko Pasha <pmashchenko@gmail.com>
2012-11-29 23:19:40 +0200

    fix animation

--- js/mapGeographic.js
+++ js/mapGeographic.js
@@ -1,27 +1,27 @@
 $(document).ready(function(){
-    var fadeSpeedFast = 200, fadeSpeedSlow =700, animateSpeed = 1000, block = 1;
+    var fadeSpeedFast = 200, fadeSpeedSlow =700, animateSpeed = 500, block = 1;
     if($.browser.msie && $.browser.version !== 9){
         fadeSpeedFast = 0;
         fadeSpeedSlow =0;
         animateSpeed = 0;
     }
-    var map = $('#mapGeographic'),
-        mapControl = $('#map-control', map),
-        mapUl = $('ul', map),
-        mapGasPipe = $('#gas-pipe', map),
-        mapGasCount = $('#gas-count', map),
-        mapPartLeft = $('#map-part-left', map),
-        mapPartRight = $('#map-part-right', map),
-        mapGasField = $('#gas-field', map),
-        mapTextGrad = $('.textGrad', map),
-        mapH2 = $('h2', map);
+    var map = $( '#mapGeographic' ),
+        mapControl = $( '#map-control', map ),
+        mapUl = $( 'ul', map ),
+        mapGasPipe = $( '#gas-pipe', map ),
+        mapGasCount = $( '#gas-count', map ),
+        mapPartLeft = $( '#map-part-left', map ),
+        mapPartRight = $( '#map-part-right', map ),
+        mapGasField = $( '#gas-field', map ),
+        mapTextGrad = $( '.textGrad', map ),
+        mapH2 = $( 'h2', map );
 
     if(block){
-        map.fadeIn(fadeSpeedSlow,function(){
-            mapControl.fadeIn(fadeSpeedFast, function(){
-                mapUl.fadeIn(fadeSpeedSlow, function(){
-                    mapGasPipe.fadeIn(fadeSpeedSlow, function(){
-                        mapGasCount.fadeIn(fadeSpeedSlow);
+        map.fadeIn( fadeSpeedSlow,function(){
+            mapControl.fadeIn( fadeSpeedFast, function(){
+                mapUl.fadeIn( fadeSpeedSlow, function(){
+                    mapGasPipe.fadeIn( fadeSpeedSlow, function(){
+                        mapGasCount.fadeIn( fadeSpeedSlow );
                         block = 0
                     });
                 });
@@ -30,44 +30,62 @@ $(document).ready(function(){
     }
 
     Cufon.replace( mapTextGrad, {
-        color: '-linear-gradient(#c2c2c2, #383838, #000000)'
+        color: '-linear-gradient( #c2c2c2, #383838, #000000 )'
     });
 
-    $('#map-control').on('click', 'a', function (){
-        if(block){
+    $( '#map-control' ).on( 'click', 'a', function (){
+        if( block ){
             return;
         }else{
             showMapSlide(this);
-            $(this).addClass('active').siblings('a').removeClass('active');
+            $( this ).addClass( 'active' ).siblings( 'a' ).removeClass( 'active' );
         }
         return false;
     });
 
     function showMapSlide( targetObj ){
         block = 1;
+        var mapLeftPosition = parseInt( mapPartLeft.css( 'left' ) );
+        var mapRightPosition = parseInt( mapPartRight.css( 'left' ) );
+        var mapStepLeft = 24;
+        var mapStepRight = 47;
 
-        if($( targetObj ).hasClass( 'mapCollapse' ) && !$( targetObj ).hasClass( 'active' ) ){
+        if($( targetObj ).hasClass( 'mapCollapse' ) && !$( targetObj).hasClass( 'active' )){
            mapUl.add( mapGasCount ).add( mapH2 ).add( mapGasPipe ).fadeOut(fadeSpeedSlow, function(){
                 mapPartLeft.add( mapPartRight ).css( 'visibility', 'visible' );
-                map.css('background', 'none');
-                var mapLeftPosition = parseInt( mapPartLeft.css('left') );
-                var mapRightPosition = parseInt( mapPartRight.css('left') );
-
-                mapPartLeft.animate({left: mapLeftPosition-24}, animateSpeed);
-                mapPartRight.animate({left: mapRightPosition+47}, animateSpeed, function(){
-                    mapTextGrad.html('104 720 000 000').siblings('p').html('запасов кубометров газа');
+                map.css('backgroundImage', 'none');
+                mapPartLeft.animate( {left: mapLeftPosition - mapStepLeft}, animateSpeed);
+                mapPartRight.animate( {left: mapRightPosition + mapStepRight}, animateSpeed, function(){
+                    mapTextGrad.html( '104 720 000 000' ).siblings( 'p' ).html( 'запасов кубометров газа' );
                     Cufon.replace( mapTextGrad, {
-                        color: '-linear-gradient(#c2c2c2, #383838, #000000)'
+                        color: '-linear-gradient( #c2c2c2, #383838, #000000 )'
                     });
-                    mapH2.html('Люди Ямала – мощная сила!').fadeIn(fadeSpeedSlow);
-                    mapGasField.fadeIn(fadeSpeedSlow);
-                    mapGasCount.fadeIn(fadeSpeedSlow);
+                    mapH2.html('Люди Ямала – мощная сила!').fadeIn( fadeSpeedSlow );
+                    mapGasField.add( mapGasCount ).fadeIn( fadeSpeedSlow );
                     block = 0;
                 });
             });
-        }else{
-
-        }
+        }else if($( targetObj ).hasClass( 'mapRecovery' ) && !$( targetObj).hasClass( 'active' )){
+            mapGasField.add( mapGasCount ).fadeOut( fadeSpeedSlow );
+            mapH2.fadeOut( fadeSpeedSlow, function(){
+                $( this ).html('Наш газ — для людей!');
+                mapPartLeft.animate( {left: mapLeftPosition + mapStepLeft}, animateSpeed );
+                mapPartRight.animate( {left: mapRightPosition - mapStepRight}, animateSpeed, function(){
+                    map.css('backgroundImage', '');
+                    mapPartLeft.add( mapPartRight ).css( 'visibility', 'hidden' );
+                    mapTextGrad.html( '185 000 000').siblings( 'p').html( 'кубометров газа доставлено адресатам' );
+                    Cufon.replace( mapTextGrad, {
+                        color: '-linear-gradient( #c2c2c2, #383838, #000000 )'
+                    });
+                    mapUl.fadeIn( fadeSpeedSlow, function(){
+                        mapGasPipe.fadeIn( fadeSpeedSlow, function(){
+                            mapGasCount.add(mapH2).fadeIn( fadeSpeedSlow );
+                            block = 0;
+                        });
+                    });
+                });
+            });
+        }else{block = 0;}
     }
 
 });
