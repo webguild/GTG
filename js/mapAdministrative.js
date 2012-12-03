8cde5e02e46d6ae5bf2673d46982f66c15d4284f
Mashchenko Pasha <pmashchenko@gmail.com>
2012-12-03 14:24:11 +0200

    fix show popup

--- js/mapAdministrative.js
+++ js/mapAdministrative.js
@@ -2,13 +2,15 @@
 $(function(){
 
 	$('map').on('mouseenter', 'area', function(event){
+        var li = $('#' + this.id + '1');
 		if($(this).hasClass('active')){
+            $('div', li).css('display', 'block');
             $(this).on('mouseleave', function(event){
-                li.css('display', 'none');
+                $('div', li).css('display', 'none');
             });
             return;
         }
-		var li = $('#' + this.id + '1');
+
 		var left = li.attr('data-left');
 		var top = li.attr('data-top');
 
@@ -18,7 +20,8 @@ $(function(){
 			'display':'block',
 			'top': top + 'px',
 			'left': left  + 'px'
-			});
+		});
+        $('div', li).css('display', 'block');
 
 		$(this).on('mouseleave', function(event){
 			li.css('display', 'none');
@@ -30,12 +33,10 @@ $(function(){
 			$('.active', li).show();
 			$(this).addClass('active').siblings().removeClass('active');
             $(this).off();
+            $(this).on('mouseleave', function(event){
+                $('div', li).css('display', 'none');
+            });
 		});		
 	});
-
-	
-
-
-
 });
 }());
